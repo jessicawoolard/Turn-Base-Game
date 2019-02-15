ccad9bba3c004f16c5ce50dc77f709d6a4e65b11
Seth Williams <swilliams9334@gmail.com>
2019-02-15 14:21:20 -0500

    images are in, some stuff has been played around with

--- fighters/static/fighters/js/main.js
+++ fighters/static/fighters/js/main.js
@@ -1,4 +1,9 @@
-
+(function ($) {
+    $(function () {
+        var csrftoken = $(“[name=csrfmiddlewaretoken]“).val();
+       function csrfSafeMethod(method) {
+       // these HTTP methods do not require CSRF protection
+       return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 
 
 class Character{
@@ -7,7 +12,6 @@ class Character{
        this.health = context.health || 100;
        this.attack1 = context.attack1;
        this.attack2 = context.attack2;
-       this.image = context.image;
     }
 }
 
@@ -26,28 +30,27 @@ class Villain extends Character{
 }
 
 
-var fiona = new Hero({playerName: "Fiona", attack1: 'hit1', attack2: 'hit2', image: 'image', specialPower: 'snatch and grab'});
+var fiona = new Hero({playerName: "Fiona", attack1: 'hit1', attack2: 'hit2', specialPower: 'snatch and grab'});
 console.log(fiona);
-var frank = new Villain({playerName: "Frank", attack1: 'hit1', attack2: 'hit2', image: 'image', villainPower: 'drunk joke here'});
+var frank = new Villain({playerName: "Frank", attack1: 'hit1', attack2: 'hit2', villainPower: 'drunk joke here'});
 console.log(frank);
+var michael = new Hero({playerName: "Michael", attack1: 'hit1', attack2: 'hit2', specialPower: 'lonely punch'});
+console.log(michael);
+var jan = new Villain({playerName: "Prison Mike", attack1: 'hit1', attack2: 'hit2', villainPower: 'drunk joke here'});
+console.log(jan);
+var will = new Hero({playerName: "Will", attack1: 'hit1', attack2: 'hit2', specialPower: 'snatch and grab'});
+console.log(will);
+var carlton = new Villain({playerName: "Carlton", attack1: 'hit1', attack2: 'hit2', villainPower: 'drunk joke here'});
+console.log(carlton);
+var zack = new Hero({playerName: "Zack Morris", attack1: 'hit1', attack2: 'hit2', specialPower: 'snatch and grab'});
+console.log(zack);
+var belding = new Villain({playerName: "Mr. Belding", attack1: 'hit1', attack2: 'hit2', villainPower: 'drunk joke here'});
+console.log(belding);
+var eric = new Hero({playerName: "Eric", attack1: 'hit1', attack2: 'hit2', specialPower: 'snatch and grab'});
+console.log(eric);
+var red = new Villain({playerName: "Red", attack1: 'hit1', attack2: 'hit2', villainPower: 'drunk joke here'});
+console.log(red);
 
-// The Office
-//     Michael Scott
-//     Jan Levinson-Gould
-//
-// Saved by the bell
-//     Zack Morris
-//     Mr. Belding
-//
-// Fresh Prince
-//     Will Smith
-//     Carlton Banks
-//
-//
-// Married with children
-//     Peggy Bundy
-//     Al Bundy
-//
-// That 70's Show
-//     Eric Forman
-//     Red Forman
+});
+
+}(jQuery));
