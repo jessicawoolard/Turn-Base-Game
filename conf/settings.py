c2f6835ad4911bb44fae8bbeb4645eb990439239
Seth Williams <swilliams9334@gmail.com>
2019-02-16 13:17:15 -0500

    sqlite

--- conf/settings.py
+++ conf/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'fighters', 'api',
     'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -77,12 +78,8 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'SitcomSparring',
-        'USER': 'MichaelScott',
-        'PASSWORD': 'safepass',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
