fe4e0f43a1064dc23640508b13de55669d68092a
Jessica <woolard.jessica@gmail.com>
2019-02-16 15:51:29 -0500

    migrated models

--- fighters/models.py
+++ fighters/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Game(models.Model):
+    heroHealth = models.IntegerField(default=100)
+    villainHealth = models.IntegerField(default=100)
+
