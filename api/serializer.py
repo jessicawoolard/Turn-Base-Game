4e86e46f74f39a29c35f43db31d6cfbc52bf3123
Jessica <woolard.jessica@gmail.com>
2019-02-18 13:57:15 -0500

    starting api

--- api/serializer.py
+++ api/serializer.py
@@ -1,1 +1,10 @@
 from rest_framework.serializers import ModelSerializer
+
+from fighters.models import Game
+
+
+class GameSerializer(ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ('heroHealth', 'villainHealth')
+        depth = 1
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import GameSerializer
+from fighters.models import Game
 
-# Create your views here.
+
+class GameViewSet(viewsets.ModelViewSet):
+    model = Game
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
