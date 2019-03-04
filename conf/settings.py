04118718367a1bf1234dc4291230c422d24c665c
Jessica <woolard.jessica@gmail.com>
2019-03-04 13:28:05 -0500

    updated static in settings

--- conf/settings.py
+++ conf/settings.py
@@ -33,7 +33,7 @@ STATICFILES_FINDERS = [
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'fighters/static'),
+    os.path.join(BASE_DIR, 'fighters/static/fighters'),
 )
 
 
